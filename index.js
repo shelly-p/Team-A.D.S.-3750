cc415baaaf1ea35d08f8e1eb7ac66d28865264f0
bracaanissa <91158785+bracaanissa@users.noreply.github.com>
2023-03-29 09:10:00 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import {
   updateDoc
 } from 'firebase/firestore'
 import {
-  getAuth,
+  getAuth, onAuthStateChanged,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword, signOut
 } from 'firebase/auth'
@@ -23,60 +23,133 @@ const firebaseConfig = {
 }
 
 // init firebase
-initializeApp(firebaseConfig)
+firebase.initializeApp(firebaseConfig)
 
 // init services
-const db = getFirestore()
-const auth = getAuth()
+//const db = getFirestore()
+//const authi = getAuth()
+const auth = firebase.auth()
+const database = firebase.database()
 
 
-// signing users up
-const signupForm = document.querySelector('.signup')
-signupForm.addEventListener('submit', (e) => {
-  e.preventDefault()
+// Set up our register function
+function register () {
+  // Get all our input fields
+  email = document.getElementById('email').value
+  password = document.getElementById('password').value
 
-  const email = signupForm.email.value
-  const password = signupForm.password.value
 
-  createUserWithEmailAndPassword(auth, email, password)
-    .then(cred => {
-      console.log('user created:', cred.user)
-      signupForm.reset()
-    })
-    .catch(err => {
-      console.log(err.message)
-    })
-})
+  // Validate input fields
+  if (validate_email(email) == false || validate_password(password) == false) {
+    alert('Email or Password is Outta Line!!')
+    return
+    // Don't continue running the code
+  }
 
-// logging in and out
-const logoutButton = document.querySelector('.logout')
-logoutButton.addEventListener('click', () => {
-  signOut(auth)
-    .then(() => {
-      console.log('user signed out')
-    })
-    .catch(err => {
-      console.log(err.message)
-    })
+  // Move on with Auth
+  auth.createUserWithEmailAndPassword(email, password)
+  .then(function() {
+    // Declare user variable
+    var user = auth.currentUser
 
-})
+    // Add this user to Firebase Database
+    var database_ref = database.ref()
 
-// logging in
-const loginForm = document.querySelector('.login')
-loginForm.addEventListener('submit', (e) => {
-  e.preventDefault()
+    // Create User data
+    var user_data = {
+      email : email,
+      last_login : Date.now()
+    }
 
-  const email = loginForm.email.value
-  const password = loginForm.password.value
+    // Push to Firebase Database
+    database_ref.child('users/' + user.uid).set(user_data)
 
-  signInWithEmailAndPassword(auth, email, password)
-    .then(cred => {
-      console.log('user logged in:', cred.user)
-      loginForm.reset()
-    })
-    .catch(err => {
-      console.log(err.message)
-    })
-    
-})
+    // DOne
+    alert('User Created!!')
+  })
+  .catch(function(error) {
+    // Firebase will use this to alert of its errors
+    var error_code = error.code
+    var error_message = error.message
 
+    alert(error_message)
+  })
+}
+
+// Set up our login function
+function login () {
+  // Get all our input fields
+  email = document.getElementById('email').value
+  password = document.getElementById('password').value
+
+  // Validate input fields
+  if (validate_email(email) == false || validate_password(password) == false) {
+    alert('Email or Password is Outta Line!!')
+    return
+    // Don't continue running the code
+  }
+
+  auth.signInWithEmailAndPassword(email, password)
+  .then(function() {
+    // Declare user variable
+    var user = auth.currentUser
+
+    // Add this user to Firebase Database
+    var database_ref = database.ref()
+
+    // Create User data
+    var user_data = {
+      last_login : Date.now()
+    }
+
+    // Push to Firebase Database
+    database_ref.child('users/' + user.uid).update(user_data)
+
+    // DOne
+    alert('User Logged In!!')
+
+  })
+  .catch(function(error) {
+    // Firebase will use this to alert of its errors
+    var error_code = error.code
+    var error_message = error.message
+
+    alert(error_message)
+  })
+}
+
+
+
+
+// Validate Functions
+function validate_email(email) {
+  expression = /^[^@]+@\w+(\.\w+)+\w$/
+  if (expression.test(email) == true) {
+    // Email is good
+    return true
+  } else {
+    // Email is not good
+    return false
+  }
+}
+
+function validate_password(password) {
+  // Firebase only accepts lengths greater than 6
+  if (password < 6) {
+    return false
+  } else {
+    return true
+  }
+}
+
+function validate_field(field) {
+  if (field == null) {
+    return false
+  }
+
+  if (field.length <= 0) {
+    return false
+  } else {
+    return true
+  }
+}
