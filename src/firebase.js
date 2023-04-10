64c7b1d6a6599b91641b6760c990160853f2bc02
shelly-p <117414886+shelly-p@users.noreply.github.com>
2023-04-09 21:22:05 -0400

    Add files via upload

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,29 @@
 // Import the necessary modules
 import { initializeApp } from "firebase/app";
-import { getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut, createUserWithEmailAndPassword } from "firebase/auth";
-import { collection, getDocs, getFirestore, query, orderBy } from "firebase/firestore";
+import { 
+  getAuth, 
+  onAuthStateChanged, 
+  signInWithEmailAndPassword, 
+  signOut, 
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInAnonymously,
+  signInWithPopup
+} from "firebase/auth";
+import { 
+  collection, 
+  getDoc , 
+  getDocs, 
+  getFirestore, 
+  query, 
+  orderBy, 
+  where, 
+  doc, 
+  setDoc,
+  onSnapshot,
+  updateDoc,
+  addDoc
+} from "firebase/firestore";
 
 // Initialize Firebase
 const firebaseConfig = {
@@ -18,51 +40,136 @@ const app = initializeApp(firebaseConfig);
 
 const db = getFirestore(app);
 const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
 
 const questionsCollectionRef = collection(db, "questions");
 
-// Create a function that handles custom authentication
-const authenticateUser = async (email, password) => {
+const signInWithGoogle = async () => {
   try {
-    const userCredential = await signInWithEmailAndPassword(auth, email, password);
-    const user = userCredential.user;
-    console.log("Authenticated user:", user);
-    // Set user as authenticated in session storage
-    window.sessionStorage.setItem("authenticatedUser", JSON.stringify(user));
+    const result = await signInWithPopup(auth, provider);
+    const user = result.user;
+    sessionStorage.setItem("username", user.displayName);
   } catch (error) {
-    console.error("Error authenticating user:", error);
+    console.error('Error signing in with Google:', error);
   }
+}
+
+// function to sign is a user anonymously
+async function guestSignIn(name) {
+  signInAnonymously(auth)
+  .then((userCredential) => {
+    const { uid } = userCredential.user;
+    const userRef = doc(db, "users", uid);
+    setDoc(userRef, {
+      name,
+      authProvider: "guest"
+    })
+      .then(() => {
+        sessionStorage.setItem("username", name);
+      })
+      .catch((error) => {
+        console.error("Error adding user to collection: ", error);
+      });
+  })
+  .catch((error) => {
+    console.error("Error registering user: ", error);
+  });
+}
+
+// function that handles custom authentication
+const authenticateUser = async (email, password) => {
+  signInWithEmailAndPassword(auth, email, password)
+  .then(async (userCredential) => {
+    const user = userCredential.user;
+    const userRef = doc(db, "users", user.uid);
+    const docSnap = await getDoc(userRef);
+    if(docSnap.exists()) {
+      const username = docSnap.data().username;
+      sessionStorage.setItem("username", username);
+    }
+    console.log("User signed in.");
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log(errorCode, ": ", errorMessage);
+  });
 };
 
-// Create a function that signs the user out
+// function that signs the user out
 const signOutUser = async () => {
   try {
     await signOut(auth);
     console.log("User signed out successfully");
     // Remove user from session storage
-    window.sessionStorage.removeItem("authenticatedUser");
+    window.sessionStorage.clear();
   } catch (error) {
     console.error("Error signing user out:", error);
   }
 };
 
-// Create a function that registers a new user
-const registerUser = async (emailRef, passwordRef) => {
+// function that registers a new user
+const registerUser = async (email, password, username) => {
+  createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    const { uid } = userCredential.user;
+    const userRef = doc(db, "users", uid);
+    setDoc(userRef, {
+      email,
+      username,
+      authProvider: "custom"
+    })
+      .then(() => {
+        sessionStorage.setItem("username", username);
+      })
+      .catch((error) => {
+        console.error("Error adding user to collection: ", error);
+      });
+  })
+  .catch((error) => {
+    console.error("Error registering user: ", error);
+  });
+};
+
+// Create a function that adds a new game to the 'GameId' collection
+const addGame = async (category, numOfQuestions) => {
+  const access = Math.floor(1000 + Math.random() * 9000); // Generate a random 4-digit number for access code
+  const userEmail = auth.currentUser.email; // Get the email of the user currently logged in
   try {
-    const userCredential = await createUserWithEmailAndPassword(auth, emailRef, passwordRef);
-    const user = userCredential.user;
-    console.log("Registered user:", user);
-    // Set user as authenticated in session storage
-    window.sessionStorage.setItem("authenticatedUser", JSON.stringify(user));
+    await setDoc(doc(db, "GameId", access), {
+      access,
+      category,
+      createdBy: userEmail,
+      numOfQuestions
+    });
+    console.log("Document written with ID: ", access);
+    return access;
   } catch (error) {
-    console.error("Error registering user:", error);
+    console.error("Error adding document: ", error);
   }
 };
 
 
-
-
-
-
-export { auth, authenticateUser, signOutUser, registerUser, onAuthStateChanged,
-   getDocs, questionsCollectionRef, query, orderBy };
+export {
+  auth, 
+  authenticateUser, 
+  collection,
+  signOutUser, 
+  registerUser, 
+  onAuthStateChanged,
+  db, 
+  doc,
+  getDocs,
+  getDoc, 
+  questionsCollectionRef, 
+  query, 
+  orderBy, 
+  where,
+  signInWithGoogle,
+  guestSignIn,
+  onSnapshot,
+  updateDoc,
+  setDoc,
+  addDoc,
+  addGame // Add this line to export the addGame function
+};
---
+++ src/pages/Game.js
@@ -0,0 +1,237 @@
+import { useState, useEffect, useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import { auth, db, doc, getDocs, where, questionsCollectionRef, query, updateDoc, onAuthStateChanged, onSnapshot } from "../firebase";
+
+
+function Timer({ onTimerEnd }) {
+  const [seconds, setSeconds] = useState(10);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSeconds((seconds) => seconds - 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    if (seconds === 0) {
+      onTimerEnd();
+      setSeconds(10);
+    }
+  }, [seconds, onTimerEnd]);
+
+  return (
+    <div>
+      <p>Timer: {seconds}</p>
+    </div>
+  );
+}
+
+ 
+
+const Game = () => {
+  const [authenticatedUser, setAuthenticatedUser] = useState(null);
+  const [questionData, setQuestionData] = useState([]);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [currentQuestion, setCurrentQuestion] = useState({});
+  const [answerOptions, setAnswerOptions] = useState([]);
+  const [selectedAnswer, setSelectedAnswer] = useState('');
+  const [isCorrect, setIsCorrect] = useState(false);
+  const [isGameOver, setIsGameOver] = useState(false);
+  const [totalPoints, setTotalPoints] = useState(0);
+  const [currentGameInfo, setCurrentGameInfo] = useState(JSON.parse(sessionStorage.getItem("gameInfo")));
+  const [username, setUsername] = useState(sessionStorage.getItem("username"));
+
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setAuthenticatedUser(user);
+
+      } else {
+        setAuthenticatedUser(null);
+        setCurrentGameInfo(null);
+      }
+    });
+    return unsubscribe;
+  }, []);
+
+  
+  useEffect(() => {
+
+    const getQuestions = async () => {
+      const questionCol = query(questionsCollectionRef, where("category", "==", "geography"));
+      const snapshot = await getDocs(questionCol);
+      const data = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        question: doc.data().question,
+        answer: doc.data().answer,
+        difficulty: doc.data().difficulty,
+      }));
+      setQuestionData(data);
+      setCurrentQuestion(randomQuestion(data));
+    };
+
+    getQuestions();
+  }, []);
+
+  useEffect(() => {
+    if (currentQuestion.id) {
+      setAnswerOptions(randomAnswerOptions(currentQuestion, questionData));
+      setSelectedAnswer('');
+      setIsCorrect(false);
+      
+    }
+  }, [currentQuestion]);
+
+  const randomQuestion = (questions) => {
+    const randomIndex = Math.floor(Math.random() * questions.length);
+    return questions[randomIndex];
+  };
+
+  const randomAnswerOptions = (question, questions) => {
+    const options = [question.answer];
+    while (options.length < 4) {
+      const randomQ = randomQuestion(questions);
+      if (randomQ.answer !== question.answer && !options.includes(randomQ.answer)) {
+        options.push(randomQ.answer);
+      }
+    }
+    shuffleArray(options);
+    return options;
+  };
+
+  const handleNextQuestion = () => {
+    
+    if (currentQuestionIndex === questionData.length - 1) {
+      setCurrentQuestion({});
+      setIsGameOver(true);
+    } else {
+      setCurrentQuestionIndex(currentQuestionIndex + 1);
+      setCurrentQuestion(questionData[currentQuestionIndex + 1]);
+      setIsCorrect(false);
+      setSelectedAnswer('');
+    }
+  };
+
+  const handleAnswer = (event) => {
+    const userAnswer = event.target.value;
+    const isAnswerCorrect = userAnswer === currentQuestion.answer.toString();
+    setSelectedAnswer(userAnswer);
+    setIsCorrect(isAnswerCorrect);
+    const points = calculatePoints(currentQuestion.difficulty);
+    if (isAnswerCorrect) {
+      setTotalPoints(totalPoints + points);
+      addPointsToCollection(totalPoints + points);
+    }
+    
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setIsCorrect(selectedAnswer === currentQuestion.answer.toString());
+  }
+
+  const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  };
+
+  const calculatePoints = (difficulty) => {
+    switch (difficulty) {
+      case 1:
+        return 100;
+      case 2:
+        return 150;
+      case 3:
+        return 200;
+      case 4:
+        return 250;
+      case 5:
+        return 500;
+      default:
+        return 0;
+    }
+  };
+
+  const addPointsToCollection = async (totalPoints) => {
+
+    const docId = sessionStorage.getItem("docId");
+
+    console.log("points: ", totalPoints);
+
+    const playerRef = doc(db, "players", docId);
+    
+    await updateDoc(playerRef, {
+      points: totalPoints
+    });
+  }
+
+
+  return (
+    <div className="container-lg">
+      <div className='row justify-content-center'>
+        <div className="col-md-5 text-center">
+          {authenticatedUser ? (
+            <>
+              <h1>Trivia Game</h1>
+              {!isGameOver && <Timer onTimerEnd={handleNextQuestion}/>}
+              <div className="card ">
+                <div className="card-header">
+                  Question {currentQuestionIndex + 1} of {questionData.length}
+                </div>
+
+                <div className="card-body">
+                  <p>Difficulty: {currentQuestion.difficulty}</p>
+                  <p>Total Points: {totalPoints}</p>
+                  <p className="display-5">{currentQuestion.question}</p>
+                </div>
+              </div>
+              <form onSubmit={handleSubmit}>
+                {answerOptions.map((option) => (
+                  <div className="d-grid gap-3" key={option}>
+                    <div className="p-2">
+                      <div className="card">
+                        <button
+                          className="btn btn-outline-primary btn-block"
+                          type="button"
+                          value={option}
+                          onClick={handleAnswer}
+                          disabled={selectedAnswer !== ''}
+                        >
+                          {option}
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </form>
+              <button
+                className="btn btn-dark"
+                onClick={handleNextQuestion}
+              >
+                Next Question
+              </button>
+              {isCorrect && <p>Correct!</p>}
+              {!isCorrect && selectedAnswer !== '' && <p>Incorrect!</p>}
+              {isGameOver && <p>Game Over!</p>}
+            </>
+          ) : (
+            <>
+              <p className="display-5 text-center">Must be logged in to play.</p>
+              <button className="btn btn-secondary btn-lg" onClick={() => navigate("/Login")}>Go to Login</button>
+            </>
+          )}
+        </div>
+      </div>
+    </div>
+
+  );
+};
+
+export default Game;
